f39a20bf19d6c90f05f5dc9e76e3c0a97a365042
jealob <jealob@gmail.com>
2018-04-01 22:40:49 -0500

    update pictures and songs, complete the js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,57 +4,74 @@
 $(document).ready(function(){
 
     // Declare global variables
-
-    // var wordList = ["memo", "paper", "printer", "shelf", "ink", "pen", "conference"];
-    var wordList = ["bustarhymes","nicliminaj","rickross","ludacris","kanyewest","thegame","nas","wayne","notoriousbig","jayz","","dmx","warreng","eminem","missyelliot","llcoolj","fiftycent","tupac", "snoppydog","drdre"];
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+    //  var wordList = ["memo", "paper", "printer", "shelf", "ink", "pen", "conference"];
+    var wordList = ["bustarhymes","nicKiminaj","rickross","ludacris","kanyewest","thegame","nas","lilwayne","notoriousbig","jayz","","dmx","warreng","eminem","missyelliott","llcoolj","fiftycent","tupac", "snoopdogg","drdre","icecube", "beyonce", "aliciakeys", "coolio", "rihanna"];
     // var wordString = "MERRY";
     var word =[];
     var allGuesses;
     var rightGuesses;
-    var wrongGuesses;
+    var wrongGuesses = '';
     var presentGuess;
     var positionInWord;
     var guessRemain;
     var wins = 0;
     var losses = 0;
+    var rapImage;
+    var rapSong;
+    var stopSongRef;
 
     // Start the Hangman game
-
     startGame();
-    var keySound;
     document.onkeyup = function(event) { //keypress callback function
         presentGuess = event.key.toUpperCase(); // Returns pressed key.
-        //Check if already guessed and return position in allGuesses
-        if (allGuesses.search(presentGuess) === -1) { //make sure not to repeat both right or wrong guesses 
+        //Check if it is an alphabet and it already guessed and return position in allGuesses, make sure not to repeat both right or wrong guesses 
+        if ((alphabet.search(presentGuess) !== -1) && (allGuesses.search(presentGuess) === -1)) { 
             allGuesses += presentGuess;
             letterChecker();    // Check if guess is right or wrong
-            if (rightGuesses === wordString) {
+            displayGame();
+            if (rightGuesses === wordString) { 
                 wins++;
-                imageDisplay = rightGuesses;
-                startGame();
+                document.querySelector("#rapper").innerHTML = "<p>Guessed Right</p>" + rightGuesses;
+                rapImage = "assets/images/" + rightGuesses + ".jpg";
+                rapSong = "assets/images/" + rightGuesses + ".mp3";
+                imageDisplay(rapImage, rightGuesses);
+                if (stopSongRef !== undefined) { // Check if a song is playing
+                    stopSongRef.pause();
+                }
+                stopSongRef = playSong(rapSong);
+                setTimeout(function() {
+                    startGame();
+                },500);               
             }
             if (guessRemain < 1) {
                 losses++;
-                imageDisplay = "";
-                startGame();
+                imageDisplay("hangman.png", hanged);
+                document.querySelector("#rapper").innerHTML = "<p>Wrong</p>" + "<p>Hanged!!!</p>";
+                playSong("assets/images/fail.mp3");
+                setTimeout(function() {
+                    startGame();
+                },500);
             }
-        }
-        displayGame();
+        }    
     };
 
     // Restarts after every round
     function startGame() {
+        wrongGuesses = ' ';
         allGuesses = "";
-        wordString  = (wordList[Math.floor(Math.random() * wordList.length)]).toUpperCase(); //Randomly selects a word from the array wordList
-        guessRemain = wordString.length; 
-        wrongGuesses = "";
         currentDisplay = [];
-        for (var i = 0; i<guessRemain; i++){
-            currentDisplay[i] = "_";    // Prints "_" to the screen to place current word
-            word[i] = wordString[i];    // Put the selected word into an array 
+        //Randomly selects a word from the array wordList
+        wordString  = (wordList[Math.floor(Math.random() * wordList.length)]).toUpperCase(); 
+        // Put the selected word into an array
+        word = wordString.split('');  
+        guessRemain = wordString.length; 
+        for (var i = 0; i < guessRemain; i++){
+            currentDisplay[i] = "_";    
+            //displays the game
         }
-        displayGame(); //displays the game
-        };
+        displayGame(); 
+    };
 
     function letterChecker() { // Check if guess is right or wrong
         positionInWord = wordString.search(presentGuess); //Check if in  word and return position in word
@@ -67,14 +84,14 @@ $(document).ready(function(){
     };
 
     function isWrong() {
-        playSound("assets/images/wrong.mp3");
-        guessRemain--; // reduce guesses remaining by one
+        keySound("assets/images/wrong.mp3");
+        guessRemain--;
         wrongGuesses += presentGuess + ","; // Add to wronglGuesses. Does not require an array or for loop
         
-    }
+    };
 
     function isRight() {
-        playSound("assets/images/right.mp3");
+        keySound("assets/images/right.mp3");
         for (var i = 0; i<word.length; i++){
             if (word[i] === presentGuess){
                 currentDisplay[i] = presentGuess;
@@ -83,15 +100,22 @@ $(document).ready(function(){
         rightGuesses = currentDisplay.join(""); // Change the array currentDisplay to string for comparison (somehow my array comparison isn't working???)
     };
 
-    function playSound(s) { // key sounds function
-        keySound = document.createElement("audio");
-        keySound.setAttribute("src", s);
-        keySound.play();
-    }
+    function playSong(s) { // key sounds function 
+        x = document.createElement("audio");
+        x.setAttribute("src", s);
+        x.play();
+        return x;
+    };
+
+    function keySound(s) { // key sounds function 
+        x = document.createElement("audio");
+        x.setAttribute("src", s);
+        x.play();
+    };
+
 
     function displayGame(){
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        
+        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user input
         var scoreBoard =
         "<p>PRESS ANY KEY TO GET STARTED!</p>" +
         "<p>WINS</p>" +
@@ -105,31 +129,16 @@ $(document).ready(function(){
         "<p>LETTERS ALREADY GUESSED</p>" +
         "<p> " + wrongGuesses + "</p>";
         
-        // var  imagePanel = <
-        
         
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = scoreBoard;
-
+        // document.querySelector("#image").innerHTML = rapImage;
     };
 
-    });
+    function imageDisplay(pic, name) {
+        var x = document.getElementById("image");
+        x.setAttribute("src", pic);
+        x.setAttribute("alt", name);
+    }
 
-
-
-    
-    // function displayImage(){ // For game picture display and sound
-        
-    //     //     switch (key) {
-    //     //         case value:
-                    
-    //     //             break;
-            
-    //     //         default:
-    //     //             break;
-    //     //     }
-    //     //     document.getElementById("picture").src = "../images/hangman-rap.png";
-    //     }
-    
-
-    // // document.querySelector("#picture").innerHTML = imagePanel;
+});
